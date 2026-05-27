6fa6a8cd45a501336d3f101ab1013a253639b9da
Rhuan Campos <gleysonrhuan@gmail.com>
2026-05-27 20:36:16 -0300

    SELECTs

--- selects.sql
+++ selects.sql
@@ -25,3 +25,51 @@ JOIN turma t ON t.id = mt.id_turma
 JOIN disciplina d ON d.id = t.id_disciplina
 WHERE d.nome = 'Algoritmos e Estrutura de Dados'
 
+
+-- 6. Consulta Complexa, qual a média de notas dos alunos por departamento,
+-- considerando apenas os departamentos onde a média geral é maior que 7.0
+
+SELECT dp.nome departamento, ROUND(AVG(mt.nota_final), 2) media_notas
+FROM matricula_turma mt
+JOIN turma t ON t.id = mt.id_turma
+JOIN disciplina d ON d.id = t.id_disciplina
+JOIN curso c ON c.id = d.id_curso
+JOIN departamento dp ON dp.id = c.id_departamento
+WHERE mt.nota_final IS NOT NULL
+GROUP BY dp.nome
+HAVING AVG(mt.nota_final) > 7.0
+
+
+-- 7. União entre tabelas sem repetições
+SELECT id_departamento FROM curso
+UNION
+SELECT id_departamento FROM professor
+
+-- 8. União entre tabelas com repetições
+SELECT id_departamento FROM curso
+UNION ALL
+SELECT id_departamento FROM professor
+
+-- 9. Subconsulta
+SELECT nome, carga_horaria
+FROM disciplina
+WHERE carga_horaria > (
+  SELECT AVG(carga_horaria) FROM disciplina
+)
+
+-- 10. Duas formas de fazer a mesma consulta
+
+SELECT DISTINCT(a.nome), a.email
+FROM aluno a
+JOIN matricula_turma mt ON mt.id_aluno = a.id
+JOIN turma t ON t.id = mt.id_turma
+WHERE t.ano = 2023
+
+SELECT nome, email
+FROM aluno
+WHERE id IN (
+  SELECT id_aluno
+  FROM matricula_turma mt
+  JOIN turma t ON t.id = mt.id_turma
+  WHERE t.ano = 2023
+)
